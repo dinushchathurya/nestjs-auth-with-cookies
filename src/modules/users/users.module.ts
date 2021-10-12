4245b7ee9509dc5a71e1c80f3c4435bc86dccff2
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-12 21:45:01 +0530

    feat: Peering User entity in users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/entities/user.entity';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forFeature(
+            [User]
+        )
+    ]
+})
+
 export class UsersModule {}
