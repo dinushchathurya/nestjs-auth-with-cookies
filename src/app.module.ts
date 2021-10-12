5210d92c2c153ce965f3742cfbc177a70c589004
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-12 20:54:53 +0530

    config: Peering ormconfig.ts in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    UsersModule
+  ],
   controllers: [],
   providers: [],
 })
