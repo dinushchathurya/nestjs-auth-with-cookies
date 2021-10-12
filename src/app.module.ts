8a3f3b2d856541d2418806e544537bfd9622049f
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-12 21:11:13 +0530

    fix: Peering missing config module import in to app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import config from '../ormconfig';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
