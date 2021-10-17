c93e27f7da32f2394fcc6684f73ae1f08cf0fbe3
dinushchathurya <dinushchathurya21@gmail.com>
2021-10-17 20:35:24 +0530

    refactor: Change orm comfiguration peering in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './modules/users/models/entities/user';
+import config from 'ormconfig';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type:'postgres',
-      host:'localhost',
-      port: 5432,
-      username: 'postgres',
-      password:'secret',
-      database:'myworlddb',
-      entities:[User],
-    }),
+    TypeOrmModule.forRoot(config),
     UsersModule
   ],
   controllers: [],
